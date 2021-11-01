66c1e326d6fd55770d04543be90d45094c82bb38
JeeSeongDeok <bh939@naver.com>
2021-11-01 20:10:32 +0900

    7. State in Compose

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,23 +53,28 @@ private fun Greeting(name: String) {
     // 구글에서 만든 디자인 시스템 Material Desing과 관련된 개념
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
+        // Remeber는 재구성을 방지함
+        val expended = remember { mutableStateOf(false) }
+        // 클릭 시 공간 확보를 위해서
+        val extraPadding = if (expended.value) 48.dp else 0.dp
         Row(modifier = Modifier.padding(24.dp)){
             // 대부분의 Compose UI(Text 및 Surface)들은 modifier 파라미터를 허용한다.
             // modifier는 부모 레이아웃 내에서 레이아웃, 동작, 표시하는 방법을 알려줌
             // Compose에는 alignEnd가 없으므로, weight로 가중치를 통해서 배치
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(extraPadding)
+            ) {
                 // TextView 대신 Composable func을 넣어 UI를 구성함
                 Text(text = "Hello,")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { /*TODO*/ }
+                onClick = { expended.value = !expended.value }
             ) {
-                Text("Show more")
+                Text(if (expended.value) "Show less" else "Show more")
             }
         }
     }
