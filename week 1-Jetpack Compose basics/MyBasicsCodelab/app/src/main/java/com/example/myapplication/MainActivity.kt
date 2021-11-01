5f3acb53f7da990b46bb9dc169683d818756db38
JeeSeongDeok <bh939@naver.com>
2021-11-01 18:00:04 +0900

    4. Tweaking the UI

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +33,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    // TextView 대신 Composable func을 넣어 UI를 구성함
-    Text(text = "Hello $name!")
+    // Surface 및 MaterialTheme는 사용자 인터페이스를 만드는데 도움이 되도록
+    // 구글에서 만든 디자인 시스템 Material Desing과 관련된 개념
+    Surface(color = MaterialTheme.colors.primary){
+        // TextView 대신 Composable func을 넣어 UI를 구성함
+        // 대부분의 Compose UI(Text 및 Surface)들은 modifier 파라미터를 허용한다.
+        // modifier는 부모 레이아웃 내에서 레이아웃, 동작, 표시하는 방법을 알려줌
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 // xml처럼 코드를 구성하면 옆에서 Preview로 확인 가능
 @Preview(showBackground = true)
