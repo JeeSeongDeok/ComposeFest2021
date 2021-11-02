c43f9a219b22638f0eb3cc7dc961477c049f51f1
JeeSeongDeok <bh939@naver.com>
2021-11-02 18:03:38 +0900

    13. Finishing touches!

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -22,7 +23,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 
 
 class MainActivity : ComponentActivity() {
@@ -63,42 +69,58 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 }
 @Composable
 private fun Greeting(name: String) {
-    // Remeber는 재구성을 방지함
-    val expended = remember { mutableStateOf(false) }
-    // animateDpAsState는 Compose에서 제공하는 애니메이션 효과
-    val extraPadding by animateDpAsState(
-        // 클릭 시 공간 확보를 위해서
-        if (expended.value) 48.dp else 0.dp,
-        // animation을 커스터마이징 가능
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    // Surface 및 MaterialTheme는 사용자 인터페이스를 만드는데 도움이 되도록
-    // 구글에서 만든 디자인 시스템 Material Desing과 관련된 개념
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)){
-            // 대부분의 Compose UI(Text 및 Surface)들은 modifier 파라미터를 허용한다.
-            // modifier는 부모 레이아웃 내에서 레이아웃, 동작, 표시하는 방법을 알려줌
-            // Compose에는 alignEnd가 없으므로, weight로 가중치를 통해서 배치
-            Column(modifier = Modifier
+        CardContent(name)
+    }
+}
+@Composable
+private fun CardContent(name:String) {
+    // Remeber는 재구성을 방지함
+    var expended by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            // animation을 커스터마이징 가능
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                // TextView 대신 Composable func을 넣어 UI를 구성함
-                Text(text = "Hello,")
-                Text(text = name)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expended) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            OutlinedButton(
-                onClick = { expended.value = !expended.value }
-            ) {
-                Text(if (expended.value) "Show less" else "Show more")
-            }
+        }
+        IconButton(onClick = { expended = !expended }) {
+            Icon(
+                imageVector = if (expended) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expended) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
         }
     }
 }
